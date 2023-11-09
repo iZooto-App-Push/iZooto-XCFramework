b145f2f34a41404e2a797a514d0b09bf326c006f
iZooto <60651012+iZooto-App-Push@users.noreply.github.com>
2023-11-09 13:26:30 +0530

    Add files via upload
    added files

---
+++ iZooto-XCFramework/Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version: 5.4
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+    let package = Package(
+        name: "iZooto-XCFramework",
+        platforms: [
+                .iOS(.v12)
+            ],
+        products: [
+            // Products define the executables and libraries a package produces, making them visible to other packages.
+            .library(
+                name: "iZooto-XCFramework",
+                targets: ["iZootoiOSSDK"]),
+        ],
+        targets: [
+            .binaryTarget(name: "iZootoiOSSDK", url: "https://swift.izooto.com/package/iZootoiOSSDK.xcframework.zip", checksum: "b8eddc9c5cce314a3148167605e979f643cb5080dc49652a0bb87a58e1363373")
+        ]
+    )
