54f4e091325ee7693649c885f540829d1ecc3d26
Amit Kumar Gupta <amit@datability.co>
2025-04-25 15:33:07 +0530

    Bug Fixed - Custom badge count

--- Package.swift
+++ Package.swift
@@ -16,6 +16,6 @@ import PackageDescription
                 targets: ["iZootoiOSSDK"]),
         ],
         targets: [
-            .binaryTarget(name: "iZootoiOSSDK", url: "https://github.com/iZooto-App-Push/iZootoiOSSDK/releases/download/2.4.3/iZootoiOSFramework.xcframework.zip", checksum: "0f0b0751874e3aa48ac5a084b5febcfb6f57949da12adea1df5b2efd8c364884")
+            .binaryTarget(name: "iZootoiOSSDK", url: "https://github.com/iZooto-App-Push/iZootoiOSSDK/releases/download/2.4.4/iZootoiOSFramework.xcframework.zip", checksum: "64a01e4f779a14f43d66185da45550e86d242e1bcb6521dc43790aa0b62f0eb5")
         ]
     )
