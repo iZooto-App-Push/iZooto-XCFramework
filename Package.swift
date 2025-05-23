6ba0a9f379fb5cc6862c60ca8afe0c75386d6981
Amit Kumar Gupta <amit@datability.co>
2025-05-23 14:57:26 +0530

    User-properites issues fixed

--- Package.swift
+++ Package.swift
@@ -16,6 +16,6 @@ import PackageDescription
                 targets: ["iZootoiOSSDK"]),
         ],
         targets: [
-            .binaryTarget(name: "iZootoiOSSDK", url: "https://github.com/iZooto-App-Push/iZootoiOSSDK/releases/download/2.4.4/iZootoiOSFramework.xcframework.zip", checksum: "64a01e4f779a14f43d66185da45550e86d242e1bcb6521dc43790aa0b62f0eb5")
+            .binaryTarget(name: "iZootoiOSSDK", url: "https://github.com/iZooto-App-Push/iZootoiOSSDK/releases/download/2.4.5/iZootoiOSFramework.xcframework.zip", checksum: "b9a5e45d25f09cd6225c89f689af050e58e39b649e3e8253bd82b269beedcc0e")
         ]
     )
