0727e496e2d5f1fa798f4d2ddbcfd684876a4e2f
Amit Kumar Gupta <amit@datability.co>
2025-10-06 13:09:45 +0530

    SPM- 2.4.6 Released

--- Package.swift
+++ Package.swift
@@ -16,6 +16,6 @@ import PackageDescription
                 targets: ["iZootoiOSSDK"]),
         ],
         targets: [
-            .binaryTarget(name: "iZootoiOSSDK", url: "https://github.com/iZooto-App-Push/iZootoiOSSDK/releases/download/2.4.5/iZootoiOSFramework.xcframework.zip", checksum: "b9a5e45d25f09cd6225c89f689af050e58e39b649e3e8253bd82b269beedcc0e")
+            .binaryTarget(name: "iZootoiOSSDK", url: "https://github.com/iZooto-App-Push/iZootoiOSSDK/releases/download/2.4.6/iZootoiOSFramework.xcframework.zip", checksum: "a18271158d53432cb6594d3067a6f95ae4ddc96ab86e874ec1f3c540fdad7db5")
         ]
     )
