c5b1ccb4d8561fd6688e32718ad08ef5ab59a33e
Amit Kumar Gupta <amit@datability.co>
2026-02-02 13:36:35 +0530

    Support multiple pids

--- Package.swift
+++ Package.swift
@@ -16,6 +16,6 @@ import PackageDescription
                 targets: ["iZootoiOSSDK"]),
         ],
         targets: [
-            .binaryTarget(name: "iZootoiOSSDK", url: "https://github.com/iZooto-App-Push/iZootoiOSSDK/releases/download/2.5.0/iZootoiOSFramework.xcframework.zip", checksum: "f73f11b4a1aa9f6e44a5d18034c1df9f721837afa72fdcbf2673ab0df893c227")
+            .binaryTarget(name: "iZootoiOSSDK", url: "https://github.com/iZooto-App-Push/iZootoiOSSDK/releases/download/3.2.1/iZootoiOSFramework.xcframework.zip", checksum: "fc1a94f0294e397a872e157f80cc63756b82d442bb4424a495ca269d211e649c")
         ]
     )
